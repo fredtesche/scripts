3b21526148199566ccbfe0f69052fde951757f94
fred <fred@fakecomputermusic.com>
2017-07-02 10:40:47 -0500

    Added phpmyadmin.conf creation

--- installbolt.sh
+++ installbolt.sh
@@ -36,7 +36,7 @@ sudo -u apache php /var/www/bolt/app/nut setup:sync
 rm bolt-latest.tar.gz
 chcon -t httpd_sys_rw_content_t /var/www/bolt/ -R
 
-# Add bolt virtualhost and restart Apache
+# Add bolt apache configs
 cat > /etc/httpd/conf.d/bolt.conf << EOL
 <VirtualHost _default_:80>
         DocumentRoot /var/www/bolt/public
@@ -48,5 +48,75 @@ cat > /etc/httpd/conf.d/bolt.conf << EOL
         </Directory>
 </VirtualHost>
 EOL
+
+# Add phpmyadmin apache configs
+cat > /etc/httpd/conf.d/phpMyAdmin.conf << EOL
+# phpMyAdmin - Web based MySQL browser written in php
+# 
+# Allows only localhost by default
+#
+# But allowing phpMyAdmin to anyone other than localhost should be considered
+# dangerous unless properly secured by SSL
+
+Alias /phpMyAdmin /usr/share/phpMyAdmin
+Alias /phpmyadmin /usr/share/phpMyAdmin
+
+<Directory /usr/share/phpMyAdmin/>
+	AddDefaultCharset UTF-8
+
+	<IfModule mod_authz_core.c>
+		# Apache 2.4
+		Require all granted
+		Order Deny,Allow
+		Allow from 192.168.0.0/16
+		Allow from 172.16.0.0/12
+		Allow from 10.0.0.0/8
+		Allow from 127.0.0.1
+	</IfModule>
+</Directory>
+
+<Directory /usr/share/phpMyAdmin/setup/>
+	<IfModule mod_authz_core.c>
+		# Apache 2.4
+		Require all granted
+		Order Deny,Allow
+		Allow from 192.168.0.0/16
+		Allow from 172.16.0.0/12
+		Allow from 10.0.0.0/8
+		Allow from 127.0.0.1
+	</IfModule>
+</Directory>
+
+# These directories do not require access over HTTP - taken from the original
+# phpMyAdmin upstream tarball
+#
+<Directory /usr/share/phpMyAdmin/libraries/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>
+
+<Directory /usr/share/phpMyAdmin/setup/lib/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>
+
+<Directory /usr/share/phpMyAdmin/setup/frames/>
+    Order Deny,Allow
+    Deny from All
+    Allow from None
+</Directory>
+
+# This configuration prevents mod_security at phpMyAdmin directories from
+# filtering SQL etc.  This may break your mod_security implementation.
+#
+#<IfModule mod_security.c>
+#    <Directory /usr/share/phpMyAdmin/>
+#        SecRuleInheritance Off
+#    </Directory>
+#</IfModule>
+EOL
+
 systemctl restart httpd.service
 
