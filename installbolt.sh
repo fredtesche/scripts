b668d1193338404ed54835e2331933453d9d2bad
fredtesche <fred@fakecomputermusic.com>
2018-01-21 12:08:10 -0600

    Updated path to epel repo

--- installbolt.sh
+++ installbolt.sh
@@ -7,14 +7,14 @@
 yum -y update
 yum -y install tcpdump nano wget
 
-# Grab the Remi and Epel repos and enable
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
+# Grab the Epel and Remi repos and enable
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 rpm -Uvh epel-release-7*.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7*.rpm
 sed -i -e '/\[remi\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo
 sed -i -e '/\[remi-php56\]/,/^\[/s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo
-rm epel-release-7-9.noarch.rpm remi-release-7.rpm
+rm epel-release-7-11.noarch.rpm remi-release-7.rpm
 
 # Install PHP, extensions, mariadb server, firewall, and phpmyadmin
 yum -y install php php-pdo php-mysqlnd php-pgsql php-gd php-mbstring php-posix php-xml mariadb mariadb-server firewalld phpmyadmin
